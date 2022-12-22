e1c9b0356cefa405e07ff1a79b26c03b9b988379
Isabel Jones <isabel.jones299@gmail.com>
2022-12-22 16:41:45 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,44 @@
+
+To do
+
+- 10:20 express backend explained
+- typescript decorators
+- server side or client side version of socket.io ?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
@@ -68,3 +109,6 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
+
 import './App.css';
-import React from "react";
+import React, { useEffect } from "react";
 import {
   BrowserRouter as Router,
   Route, Routes,
@@ -8,18 +8,37 @@ import {
 } from "react-router-dom";
 import ScrabbleBoard from './components/ScrabbleBoard.jsx'
 
+import styled from "styled-components"
+import io from "socket.io-client"
+import socketService from "./services/socketService/index.ts"
+
+
+const MainContainer = styled.div``
+const AppContainer = styled.div``
+
 function App() {
+  const connectSocket = () => {
+    const socket = io("http://localhost:9000")
+  };
+
+  useEffect(() => {
+    connectSocket();
+  }, [])
+
   return (
-    <div className="App">
+    <AppContainer>
+      <MainContainer>
+        <p>word</p>
+        <div className="App">
 
-
-      <Router>
-        <Routes>
-          <Route path='/' element={<ScrabbleBoard />} />
-        </Routes>
-      </Router>
-
-    </div>
+          <Router>
+            <Routes>
+              <Route path='/' element={<ScrabbleBoard />} />
+            </Routes>
+          </Router>
+        </div>
+      </MainContainer>
+    </AppContainer>
   );
 }
 
